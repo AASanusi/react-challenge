3d0e8ca04d63e37dadd77d24519501ee24dec197
AASanusi <alaminm.sanusi@gmail.com>
2023-01-15 13:21:11 +0000

    Intial commit and add components

---
+++ my-app/src/components/Sidebar.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+// const SideBar = () => {
+//     return <h1>It works!</h1>
+// }
+
+function SideBar() {
+    return <div>
+        <a>My Photos</a>
+        <a>My Illustrations</a>
+        <a>My Paintings</a>
+    </div>
+}
+
+export default SideBar;
+
